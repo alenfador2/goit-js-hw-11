3d47bf666c555f8d6e11ef82ed414e902edbb916
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2023-12-15 23:32:26 +0100

    Update 1.3

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,19 +1,11 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
-axios.defaults.headers.common['x-api-key'] =
-  '41281960-4f851dde922e1c31c346e4445';
-
 const gallery = document.querySelector('.gallery');
 const searchButton = document.querySelector('.search-btn');
 const input = document.querySelector('input[type="text"]');
 const baseUrl = 'https://pixabay.com/api/';
 
-const headers = new Headers({
-  'Content-Type': 'application/json',
-  'X-Custom-Header': 'custom value',
-  'Access-Control-Allow-Origin': '*',
-});
 const options = {
   params: {
     key: '41281960-4f851dde922e1c31c346e4445',
@@ -27,9 +19,29 @@ const options = {
 const fetchImg = async (baseUrl, options) => {
   try {
     const response = await axios.get(baseUrl, options);
-    const photos = await response.data;
-    console.log(photos);
-    return photos;
+    const data = response.data;
+    console.log(data);
+    const hits = data.hits;
+    const galleryItem = hits.map(item => {
+       return `<div class="photo-card">
+            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" class = "image"/>
+            <div class="info">
+              <p class="info-item">
+                <b>Likes: ${item.likes}</b>
+              </p>
+              <p class="info-item">
+                <b>Views: ${item.views}</b>
+              </p>
+              <p class="info-item">
+                <b>Comments: ${item.comments}</b>
+              </p>
+              <p class="info-item">
+                <b>Downloads: ${item.downloads}</b>
+              </p>
+            </div>
+          </div>`;
+    });
+    gallery.insertAdjacentHTML('afterbegin', galleryItem.join(''));
   } catch (error) {
     console.error(error);
   }
@@ -38,5 +50,6 @@ const fetchImg = async (baseUrl, options) => {
 searchButton.addEventListener('click', ev => {
   ev.preventDefault();
   options.params.q = input.value;
+  gallery.innerHTML = '';
   fetchImg(baseUrl, options);
 });
