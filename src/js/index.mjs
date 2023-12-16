149ed6054a769f91d6f70e61c5ae8d52e2332586
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2023-12-16 01:28:47 +0100

    Update 1.4 small fixes

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -6,6 +6,8 @@ const searchButton = document.querySelector('.search-btn');
 const input = document.querySelector('input[type="text"]');
 const baseUrl = 'https://pixabay.com/api/';
 
+let page = 1;
+const perPage = 40;
 const options = {
   params: {
     key: '41281960-4f851dde922e1c31c346e4445',
@@ -13,17 +15,27 @@ const options = {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page: page,
+    per_page: perPage,
   },
 };
 
-const fetchImg = async (baseUrl, options) => {
+const fetchPosts = async (baseUrl, options) => {
   try {
     const response = await axios.get(baseUrl, options);
     const data = response.data;
     console.log(data);
+    const totalHits = data.totalHits;
     const hits = data.hits;
-    const galleryItem = hits.map(item => {
-      return `<div class="photo-card">
+    if (hits.length === 0) {
+      Notiflix.Report.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    } else {
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+      const galleryItem = hits.map(item => {
+        return `<div class="photo-card">
             <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" class = "image"/>
             <div class="info">
               <p class="info-item">
@@ -40,8 +52,26 @@ const fetchImg = async (baseUrl, options) => {
               </p>
             </div>
           </div>`;
-    });
-    gallery.insertAdjacentHTML('afterbegin', galleryItem.join(''));
+      });
+      gallery.insertAdjacentHTML('afterbegin', galleryItem.join(''));
+      const button = document.createElement('button');
+      button.type = 'button';
+      button.classList.add('load-more');
+      button.textContent = 'Load more';
+      gallery.append(button);
+      button.addEventListener('click', async () => {
+        try {
+          page += 1;
+          options.params.page = page;
+          const posts = await fetchPosts(baseUrl, options);
+          if (page > 1) {
+            button.textContent = 'Fetch more posts';
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    }
   } catch (error) {
     console.error(error);
   }
@@ -51,5 +81,6 @@ searchButton.addEventListener('click', ev => {
   ev.preventDefault();
   options.params.q = input.value;
   gallery.innerHTML = '';
-  fetchImg(baseUrl, options);
+  page = 1;
+  fetchPosts(baseUrl, options);
 });
