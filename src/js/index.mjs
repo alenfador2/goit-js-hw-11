7385e901ccfe8b7da5750b4c32caca4ca488d4ae
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2023-12-16 03:16:17 +0100

    Update 1.5

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
 const searchButton = document.querySelector('.search-btn');
@@ -8,6 +10,8 @@ const baseUrl = 'https://pixabay.com/api/';
 
 let page = 1;
 const perPage = 40;
+let button;
+var lightbox = new SimpleLightbox('.image a', {});
 const options = {
   params: {
     key: '41281960-4f851dde922e1c31c346e4445',
@@ -19,7 +23,6 @@ const options = {
     per_page: perPage,
   },
 };
-
 const fetchPosts = async (baseUrl, options) => {
   try {
     const response = await axios.get(baseUrl, options);
@@ -36,7 +39,7 @@ const fetchPosts = async (baseUrl, options) => {
       Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
       const galleryItem = hits.map(item => {
         return `<div class="photo-card">
-            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" class = "image"/>
+            <a class = "ancor" href = "${item.previewURL}"><img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" class = "image"/></a>
             <div class="info">
               <p class="info-item">
                 <b>Likes: ${item.likes}</b>
@@ -53,29 +56,52 @@ const fetchPosts = async (baseUrl, options) => {
             </div>
           </div>`;
       });
-      gallery.insertAdjacentHTML('afterbegin', galleryItem.join(''));
-      const button = document.createElement('button');
-      button.type = 'button';
-      button.classList.add('load-more');
-      button.textContent = 'Load more';
-      gallery.append(button);
-      button.addEventListener('click', async () => {
-        try {
-          page += 1;
-          options.params.page = page;
-          const posts = await fetchPosts(baseUrl, options);
-          if (page > 1) {
-            button.textContent = 'Fetch more posts';
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      });
+      gallery.insertAdjacentHTML('beforeend', galleryItem.join(''));
+    }
+    if (totalHits > page * perPage) {
+      addMoreButton();
     }
   } catch (error) {
     console.error(error);
   }
 };
+
+function addMoreButton() {
+  if (!button) {
+    button = document.createElement('button');
+    button.type = 'button';
+    button.classList.add('load-more');
+    button.textContent = 'Load more';
+    gallery.append(button);
+  } else {
+    button.remove();
+    button.textContent = 'Fetch more posts';
+    gallery.append(button);
+  }
+
+  button.addEventListener('click', async () => {
+    try {
+      page += 1;
+      options.params.page = page;
+      lightbox.refresh();
+      await fetchPosts(baseUrl, options);
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+      if (totalHits <= page * perPage) {
+        button.remove();
+        button.style.display = 'none';
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
 
 searchButton.addEventListener('click', ev => {
   ev.preventDefault();
